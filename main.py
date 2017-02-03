31dcb86da5002aeb6a469c9a7c9f661c13e08113
Heather Cunningham <kcredwolf@gmail.com>
2017-02-02 20:24:57 -0600

    added add.html template and modified main.py to render confirmation messaage from add.html

--- main.py
+++ main.py
@@ -56,8 +56,9 @@ class AddMovie(webapp2.RequestHandler):
 
         # TODO 1
         # Use a template to render the confirmation message
-
-        self.response.write("Under construction...")
+        t = jinja_env.get_template("add.html")
+        content = t.render(new_movie=new_movie)
+        self.response.write(content)
 
 
 class CrossOffMovie(webapp2.RequestHandler):
